d2a76cc8c509928f8cc0414bdaccfeb48590b3db
Rigel Chan <rigel_chan@hotmail.com>
2022-07-04 15:55:04 -0600

    Update PlayerMovement.cs

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rb;
+    float horizontal;
+    float vertical;
+    public int speed = 30;
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    public void FixedUpdate()
+    {
+
+        if (horizontal != 0 && vertical != 0)
+        {
+            rb.velocity = new Vector2(horizontal * speed / 1.3f, vertical * speed / 1.3f);
+        }
+        else
+        {
+            rb.velocity = new Vector2(horizontal * speed, vertical * speed);
+        }
     }
 }
